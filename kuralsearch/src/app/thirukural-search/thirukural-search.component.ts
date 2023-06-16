907b11494e68fdf4ed63a33cea97b11bd3b57c95
Sherin40 <sheringod563@gmail.com>
2023-06-16 07:53:21 +0530

    finalcommit

--- kuralsearch/src/app/thirukural-search/thirukural-search.component.ts
+++ kuralsearch/src/app/thirukural-search/thirukural-search.component.ts
@@ -8,30 +8,30 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./thirukural-search.component.css']
 })
 export class ThirukuralSearchComponent {
-  searchTerm!:string;
+  searchTerm!: string;
   kural: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-search() {
-  if (this.searchTerm) {
-    const apiUrl = `https://api-thirukkural.vercel.app/api?num=${this.searchTerm}`;
-    this.http.get(apiUrl).subscribe(
-      (response: any) => {
-        console.log(response);
-        this.kural = response;
-      },
-      (error) => {
-        console.log(error);
-        alert('Sorry! Only 1330 kurals available in Thirukural');
-      }
-    );
-  } 
-}
+  search() {
+    if (this.searchTerm) {
+      const apiUrl = `https://api-thirukkural.vercel.app/api?num=${this.searchTerm}`;
+      this.http.get(apiUrl).subscribe(
+        (response: any) => {
+          console.log(response);
+          this.kural = response;
+        },
+        (error) => {
+          console.log(error);
+          alert('Sorry! Only 1330 kurals available in Thirukural');
+        }
+      );
+    }
+  }
 
 
-clear() {
-  this.searchTerm = ''; // Clear the search term
-  this.kural = null; // Clear the kural object
-}
+  clear() {
+    this.searchTerm = ''; // Clear the search term
+    this.kural = null; // Clear the kural object
+  }
 }
